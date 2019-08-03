58b9ddf3f5867adad22838744199ed87ddefc774
Doug Zongker <dougz@isotropic.org>
2019-08-03 12:56:16 -0700

    bypass load balancer in preview

--- scripts/run_preview.sh
+++ scripts/run_preview.sh
@@ -3,7 +3,7 @@
 
 exec python3.7 tools/preview.py \
      --credentials misc/hunt2020-20386e110718.json \
-     --public_host snellen-preview.isotropic.org \
+     --public_host snellen-preview.storage.googleapis.com \
      --template_path html \
      --event_dir test_event \
      "$@"
